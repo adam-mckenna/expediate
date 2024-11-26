0b75dc5c401fa7aec28859a9f20adfd4b027569d
Adam McKenna <adam-mckenna@outlook.com>
2024-11-26 11:19:11 +0700

    feat: added 'timeline' to journal

--- app/journal.tsx
+++ app/journal.tsx
@@ -3,9 +3,11 @@ import { BasicText } from '@/components/BasicText'
 
 import {
   useFonts,
+  Inter_300Light,
   Inter_400Regular,
   Inter_500Medium,
   Inter_600SemiBold,
+  Inter_900Black,
 } from '@expo-google-fonts/inter'
 import { List } from 'react-native-paper'
 
@@ -31,18 +33,27 @@ const DummyData = [
     score: -1,
     occasion: 'breakfast',
   },
+  {
+    id: '4',
+    title: 'Table sugar',
+    description: '1 serving',
+    score: -1,
+    occasion: 'lunch',
+  },
 ]
 
 const Journal = () => {
   let [fontsLoaded] = useFonts({
+    Inter_300Light,
     Inter_400Regular,
     Inter_500Medium,
     Inter_600SemiBold,
+    Inter_900Black,
   })
 
   const getTotalBreakfast = () =>
-    DummyData.filter((item) => item.occasion == 'breakfast').reduce(
-      (accumulator, current) => accumulator + current.score,
+    DummyData.filter(({ occasion }) => occasion == 'breakfast').reduce(
+      (accumulator, { score }) => accumulator + score,
       0,
     )
 
@@ -52,7 +63,13 @@ const Journal = () => {
 
     // todo: add chevron
     return (
-      <Text style={{ color: isPositive ? '#00CA2C' : '#F02835' }}>
+      <Text
+        style={{
+          color: isPositive ? '#00CA2C' : '#F02835',
+          fontSize: 10,
+          lineHeight: 10,
+        }}
+      >
         {isPositive ? '+' : '-'}
         {score}
       </Text>
@@ -78,63 +95,80 @@ const Journal = () => {
 
           <Text style={styles.h2}>Let's break it down</Text>
 
-          <Text style={styles.h3}>Breakfast {getBreakfastTotalString()} </Text>
+          <View style={styles.occasion}>
+            <View style={styles.timeline}>
+              <View style={styles.ball}></View>
+              <View style={styles.line}></View>
+            </View>
+            <View>
+              <Text style={styles.h3}>
+                Breakfast {getBreakfastTotalString()}{' '}
+              </Text>
 
-          <View>
-            {DummyData.map((item) => (
-              <List.Item
-                key={item.id}
-                title={
-                  <Text style={{ display: 'flex', alignItems: 'flex-start' }}>
-                    <Text>{item.title}</Text>
-                    {
+              <View>
+                {DummyData.map((item) => (
+                  <List.Item
+                    key={item.id}
+                    title={
                       <Text
-                        style={{
-                          color: item.score > 0 ? '#00CA2C' : '#F02835',
-                          fontSize: 10,
-                          marginLeft: 2,
-                          // todo: fix this
-                          lineHeight: 'normal' as any,
-                        }}
+                        style={{ display: 'flex', alignItems: 'flex-start' }}
                       >
-                        {item.score > 1 ? '+' : ''}
-                        {item.score}
+                        <Text>{item.title}</Text>
+                        {
+                          <Text
+                            style={{
+                              color: item.score > 0 ? '#00CA2C' : '#F02835',
+                              fontSize: 7,
+                              marginLeft: 3,
+                              // todo: type fix
+                              lineHeight: 'normal' as any,
+                            }}
+                          >
+                            {/* todo: add chevron */}
+                            {item.score > 1 ? '+' : ''}
+                            {item.score}
+                          </Text>
+                        }
                       </Text>
                     }
-                  </Text>
-                }
-                contentStyle={{
-                  paddingLeft: 8,
-                }}
-                description={item.description}
-                titleStyle={{
-                  fontSize: 14,
-                  lineHeight: 16,
-                  fontWeight: 500,
-                  fontFamily: 'Inter_500Medium',
-                  letterSpacing: -0.6,
-                }}
-                descriptionStyle={{
-                  color: '#767676',
-                  fontSize: 10,
-                  lineHeight: 12,
-                  marginTop: 3,
-                  letterSpacing: -0.6,
-                }}
-                left={() => (
-                  <View
-                    style={{
-                      backgroundColor: item.score > 0 ? '#CFFFD9' : '#FFCAD2',
-                      width: 32,
-                      height: 32,
-                      borderRadius: 6,
+                    contentStyle={{
+                      paddingLeft: 8,
                     }}
-                  >
-                    {/* todo: add SVG icons */}
-                  </View>
-                )}
-              />
-            ))}
+                    style={{
+                      padding: 0,
+                    }}
+                    description={item.description}
+                    titleStyle={{
+                      fontSize: 14,
+                      lineHeight: 16,
+                      fontWeight: 500,
+                      fontFamily: 'Inter_500Medium',
+                      letterSpacing: -0.6,
+                    }}
+                    descriptionStyle={{
+                      color: '#767676',
+                      fontSize: 10,
+                      lineHeight: 12,
+                      marginTop: 3,
+                      letterSpacing: -0.6,
+                    }}
+                    left={() => (
+                      <View
+                        style={{
+                          backgroundColor:
+                            item.score > 0 ? '#CFFFD9' : '#FFCAD2',
+                          width: 32,
+                          height: 32,
+                          borderRadius: 6,
+                        }}
+                      >
+                        {/* todo: add SVG icons */}
+                      </View>
+                    )}
+                  />
+                ))}
+              </View>
+            </View>
           </View>
         </>
       ) : (
@@ -177,9 +211,32 @@ const styles = StyleSheet.create({
     fontWeight: 600,
     fontSize: 16,
     lineHeight: 19,
-    marginBottom: 6,
+    marginBottom: 4,
     letterSpacing: -0.6,
+    display: 'flex',
+  },
+  occasion: {
     marginTop: 12,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  timeline: {
+    marginRight: 10,
+    marginTop: 4,
+    height: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  ball: {
+    width: 9,
+    height: 9,
+    borderRadius: '100%',
+    backgroundColor: '#FD7121',
+  },
+  line: {
+    width: 1,
+    height: '100%',
+    backgroundColor: '#FD7121',
   },
 })
 
